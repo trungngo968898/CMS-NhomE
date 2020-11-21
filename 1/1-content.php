f359e06a9e2cae72651a968a74ca53dc2fbb4574
dev-xuanphu <phuphamxuan278@gmail.com>
2020-11-21 14:21:22 +0700

    update

--- 1/1-content.php
+++ 1/1-content.php
@@ -18,10 +18,8 @@ $url_path = str_replace('\\', '/', $url_path);
                     <nav class="nav-menu d-none d-lg-block">
                     <ul>
                         <li class="active"><a href="#">Home</a></li>
-                        <li><a href="#about">About</a></li>
                         <li><a href="#services">Services</a></li>
                         <li><a href="#portfolio">Portfolio</a></li>
-                        <li><a href="#team">Team</a></li>
                         <li class="drop-down">
                         <a href="">Drop Down</a>
                         <ul>
